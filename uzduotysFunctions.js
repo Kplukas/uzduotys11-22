a1f4f68f999beecd01a174f6f4202e457da62204
Kplukas <p.kristupas.p@gmail.com>
2022-11-24 15:02:05 +0200

    Class work and problem solving

---
+++ uzduotysFunctions.js
@@ -0,0 +1,18 @@
+// 1.Funkcija spausdinamas tekstas
+function tekstas(x) {
+    console.log(x);
+}
+tekstas ('tekstas');
+// 2. Argumentas kiek kartu spausdint
+function tekstasKartai(x) {
+    kartai = 3;
+    console.log(x.repeat(kartai));
+}
+tekstasKartai('tekstas');
+// 4. Dalijasi be liekanos
+function beLiekanos(a){
+    remainder = a / b;
+    if(b = 1 || a = b){
+        
+    }
+}
