f9750eed86a346cfe938f98e4265819072731743
Kplukas <p.kristupas.p@gmail.com>
2022-11-29 20:33:52 +0200

    useState

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -3,22 +3,30 @@ import './App.scss';
 
 function App() {
 
-    const[color, setColor] = useState('crimson');
+    const[color, setColor] = useState(true);
 
-    const now = () => {
-        console.log('now')
-    }
+    const[count, setCount] = useState(1);
+
+    {/*     const now = () => {
+                console.log('now')
+            } 
+    */}
 
     const change = () => {
-        setColor('blue');
+        setColor(!color);
+    }
+
+    const add = () => {
+        setCount(countNow => countNow +1)
     }
 
     return (
         <div className="App">
             <div className="App-header">
-                <h1 style={{color: color}}>Bebras</h1>
+                <h1 style={{color: color ? 'red' : 'blue'}}>Bebras {count}</h1>
                 <button onClick={change} >Bebras color change</button>
-                <button onClick={now} >Press on 9:00PM</button>
+                <button onClick={add} >Add</button>
+                {/* <button onClick={now} >Press on 9:00PM</button> */}
             </div>
 
         </div>
