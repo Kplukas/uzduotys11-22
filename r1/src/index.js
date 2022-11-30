3e9b07165edef7ecb27bab8e8780aa19ced27af5
Kplukas <p.kristupas.p@gmail.com>
2022-11-30 12:06:03 +0200

    Kvad move buttons

--- r1/src/index.js
+++ r1/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './006.jsx';
+import App from './007.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
