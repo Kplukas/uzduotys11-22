628b6bd3d8b24eddd4356dbe79b57b64314d3fe8
Kplukas <p.kristupas.p@gmail.com>
2022-11-30 13:57:25 +0200

    Squares in local storage

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.scss';
 import Sq from './components/006/Sq';
@@ -16,22 +17,35 @@ function App() {
     //    {name:'Bobikas', color:'brown', type: 'dog'}
     //];
 
-    const[sq, setSq] = useState([]);
-    const[cat, setCat] = useState('');
+    const[sq, setSq] = useState(null);
+    //const[cat, setCat] = useState('');
 
     const add = () => {
         setSq(s => [...s, { id: rand(100000, 999999), color: randCol()}]);
     }
+    useEffect(() => {
+        if (null === sq) {
+            const kv = localStorage.getItem('kv');
+            if (null === kv) {
+                setSq([]);
+            } else {
+                setSq(JSON.parse(kv));
+            }
+        } else{
+            localStorage.setItem('kv', JSON.stringify(sq));
+        }
+    },[sq]);
 
-    const write = () => {
-        localStorage.setItem('myCat',JSON.stringify({cat: 'Big and Black', color: 'red'}) )
-    }
-    const read = () => {
-        setCat(JSON.parse(localStorage.getItem('myCat')));
-    }
-    const remove = () => {
-        localStorage.removeItem('myCat')
-    }
+
+    //const write = () => {
+    //    localStorage.setItem('myCat',JSON.stringify({cat: 'Big and Black', color: 'red'}) )
+    //}
+    //const read = () => {
+    //    setCat(JSON.parse(localStorage.getItem('myCat')));
+    //}
+    //const remove = () => {
+    //    localStorage.removeItem('myCat')
+    //}
 
     return (
         <div className="App">
@@ -39,14 +53,15 @@ function App() {
                 {/* mas.map((a, i) => a.type ==='cat' ? <Cat key={i} cat={a}/> : <Dog key={i} dog={a}/>) */}
                 <div className="bin">
                     {
-                        sq.map((square) => <Sq key={square.id} square={square} setSq={setSq}/>)
+                        sq?.map((square) => <Sq key={square.id} square={square} setSq={setSq}/>)
                     }
                 </div>
-                <h2 style={{color: cat?.color}}>{cat?.cat}</h2>
                 <button onClick={add}>ADD</button>
+                <h6>Click on Square to DELETE</h6>
+                {/*<h2 style={{color: cat?.color}}>{cat?.cat}</h2>
                 <button onClick={write}>Write</button>
                 <button onClick={read}>Read</button>
-                <button onClick={remove}>Remove</button>
+                <button onClick={remove}>Remove</button>*/}
             </div>
 
         </div>
